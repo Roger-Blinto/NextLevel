4da57060e4360be135f8b537a6abdd260b134a70
Roger Jönsson <roger.jonsson@blinto.se>
2025-08-29 15:10:24 +0200

    Revert "Less complicated threading"
    This reverts commit af75bda2b170c657cf1b05587bdc43a071e12dcd.

--- Sources/NextLevel.swift
+++ Sources/NextLevel.swift
@@ -310,16 +310,16 @@ public class NextLevel: NSObject {
     /// The current orientation of the device.
     public var deviceOrientation: NextLevelDeviceOrientation = .portrait {
         didSet {
-			assert(Thread.isMainThread)
-
             automaticallyUpdatesDeviceOrientation = false
 
             guard deviceOrientation != oldValue else { return }
 
-			deviceDelegate?.nextLevelDeviceOrientationWillChange(self)
+            DispatchQueue.main.async {
+                self.deviceDelegate?.nextLevelDeviceOrientationWillChange(self)
+            }
 
 			_sessionQueue.sync {
-				updateVideoOrientation(deviceOrientation)
+				updateVideoOrientation()
 			}
         }
     }
@@ -1335,7 +1335,7 @@ extension NextLevel {
         }
     }
 
-	internal func updateVideoOrientation(_ deviceOrientation: AVCaptureVideoOrientation? = nil) {
+    internal func updateVideoOrientation() {
         if let session = self._recordingSession {
             if session.currentClipHasAudio == false && session.currentClipHasVideo == false {
                 session.reset()
@@ -1343,7 +1343,7 @@ extension NextLevel {
         }
 
         var didChangeOrientation = false
-		let currentOrientation = deviceOrientation ?? self.deviceOrientation
+		let currentOrientation = self.deviceDelegate?.nextLevelCurrentDeviceOrientation?() ?? AVCaptureVideoOrientation.avorientationFromUIDeviceOrientation(UIDevice.current.orientation)
 
         if let videoOutput = self._videoOutput, let videoConnection = videoOutput.connection(with: AVMediaType.video) {
             if videoConnection.isVideoOrientationSupported && videoConnection.videoOrientation != currentOrientation {
